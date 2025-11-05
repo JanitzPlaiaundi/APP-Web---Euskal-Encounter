b092bed835a0b7bb7e7c840005972d8e620a0f27
Janitz <ikdbs@plaiaundi.net>
2025-11-05 10:56:06 +0100

    Crear menu header

---
+++ Recursos/JSConjuntos/menuHeader.js
@@ -0,0 +1,39 @@
+let containerEnlaces = document.getElementById("botonMenuMovil")
+let headerContainer = document.querySelector("header")
+let imagenMenu = document.getElementById("fotoMenuMoviles")
+
+let defaultMenuMovil = false
+let menuMovil = document.getElementById("menuMovil")
+headerContainer.appendChild(menuMovil)
+menuMovil.style.transform = "translateY(-100vh)"
+
+
+function menuParaMoviles() {
+    containerEnlaces.addEventListener("click", () => {
+        if (window.innerWidth < 1024) {
+            if (defaultMenuMovil == false) {
+                menuMovil.style.transform = "translateY(0vh)"
+                defaultMenuMovil = true
+                imagenMenu.src = "Recursos/Imagenes/BOTON_x.png"
+            } else {
+                menuMovil.style.transform = "translateY(-100vh)"
+                defaultMenuMovil = false
+                imagenMenu.src = "Recursos/Imagenes/BOTON_info.png"
+            }
+        } else {
+            defaultMenuMovil = false
+        }
+
+    })
+
+
+}
+menuParaMoviles()
+
+addEventListener("resize", () => {
+    defaultMenuMovil = false
+    menuMovil.style.transition = "0s"
+    menuMovil.style.width = `${innerWidth}`
+    menuMovil.style.transform = "translateY(-100vh)"
+    imagenMenu.src = "Recursos/Imagenes/BOTON_info.png"
+})
